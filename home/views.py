4d3e97bb827ca46056f58d02eec57c97b1bb0fe9
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-09-09 13:42:55 +0000

    about us page added

--- home/views.py
+++ home/views.py
@@ -8,3 +8,10 @@ def index(request):
     A view to return the index page
     """
     return render(request, 'home/index.html')
+
+
+def about_us(request):
+    """
+    A view to return the about us page
+    """
+    return render(request, 'home/about-us.html')
