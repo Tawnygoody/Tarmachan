fd1325d2447ef8d55a49e3b01facb309c3ba7998
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-09-14 10:35:45 +0000

    customized admin

--- products/admin.py
+++ products/admin.py
@@ -3,8 +3,43 @@ from .models import (
     Product, MasterCategory, ProductCategory, ProductSubCategory
 )
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'master_category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class MasterCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ProductCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ProductSubCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(MasterCategory)
-admin.site.register(ProductCategory)
-admin.site.register(ProductSubCategory)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(MasterCategory, MasterCategoryAdmin)
+admin.site.register(ProductCategory, ProductCategoryAdmin)
+admin.site.register(ProductSubCategory, ProductSubCategoryAdmin)
