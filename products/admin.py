6c5a7dd04072078da6e89bfe2207cc22990f5a55
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-09-14 10:24:04 +0000

    added models and fixtures

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (
+    Product, MasterCategory, ProductCategory, ProductSubCategory
+)
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(MasterCategory)
+admin.site.register(ProductCategory)
+admin.site.register(ProductSubCategory)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,146 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MasterCategory(models.Model):
+    """
+    This allows the grouping of products for men,
+    women & equipment.
+    """
+
+    class Meta:
+        verbose_name_plural = 'Master Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_master_friendly_name(self):
+        return self.friendly_name
+
+
+class ProductCategory(models.Model):
+    """
+    This allows the grouping of products for
+    product categories
+    """
+
+    class Meta:
+        verbose_name_plural = 'Product Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_product_friendly_name(self):
+        return self.friendly_name
+
+
+class ProductSubCategory(models.Model):
+    """
+    This allows the grouping of products for
+    product sub categories
+    """
+
+    class Meta:
+        verbose_name_plural = 'Product Sub Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_product_sub_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """
+    Product Model contains the detailed product information
+    with foreign keys to the category models
+    """
+    sku = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+    )
+    name = models.CharField(
+        max_length=254,
+    )
+    colour = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+    )
+    description1 = models.TextField(
+        max_length=200,
+    )
+    description2 = models.TextField(
+        null=True,
+        blank=True
+    )
+    spec1 = models.TextField(
+        max_length=150,
+        null=True,
+        blank=True
+    )
+    spec2 = models.TextField(
+        max_length=150,
+        null=True,
+        blank=True
+    )
+    spec3 = models.TextField(
+        max_length=150,
+        null=True,
+        blank=True
+    )
+    spec4 = models.TextField(
+        max_length=150,
+        null=True,
+        blank=True
+    )
+    price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2
+    )
+    rating = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
+    sizes = models.BooleanField(
+        default=False,
+        null=True,
+        blank=True
+    )
+    master_category = models.ForeignKey(
+        'MasterCategory',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL)
+    product_category = models.ForeignKey(
+        'ProductCategory',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    product_sub_category = models.ForeignKey(
+        'ProductSubCategory',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    image = models.ImageField(
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
