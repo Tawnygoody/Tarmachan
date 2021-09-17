6a9702b92516b3555e2fe333acf693768f4c812d
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-09-17 16:12:28 +0000

    change to products page header

--- tarmachan/settings.py
+++ tarmachan/settings.py
@@ -155,8 +155,8 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-FREE_DELIVERY_THRESHOLD = 50
-STANDARD_DELIVERY_PERCENTAGE = 5
+FREE_DELIVERY_THRESHOLD = 60
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
