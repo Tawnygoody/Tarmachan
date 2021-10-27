ddbfe9f646127da6b583809143132a258c5d9278
Tawnygoody <chrisgoodfellow1@hotmail.com>
2021-10-27 09:03:58 +0000

    removed secret key and set debug

--- tarmachan/settings.py
+++ tarmachan/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y8_a49r$^uhhi5n$!irl=e=kz_g(u&gd=yaox#(ek&gu8q_!jc'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['tarmachan.herokuapp.com', 'localhost']
 
